248fedc639852ce9c5b358d3b4dda73cc6ed5a11
Bogenbai Bayzharassov <51875563+thatoranzhevyy@users.noreply.github.com>
2021-03-25 08:49:57 +0600

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const readme = `\
 
 ⏳ Year progress: ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
+You can also watch the progress on [twitter](https://github.com/joemccann/dillinger)
 ---
 
 [![Progress Bar CI](https://github.com/thatoranzhevyy/thatoranzhevyy/actions/workflows/node.js.yml/badge.svg)](https://github.com/thatoranzhevyy/thatoranzhevyy/actions/workflows/node.js.yml)
