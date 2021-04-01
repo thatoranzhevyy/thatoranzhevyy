523aee5aa522db45383e5e7d67783165ab9f4712
Bogenbai Bayzharassov <51875563+thatoranzhevyy@users.noreply.github.com>
2021-04-01 11:13:29 +0600

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const readme = `\
 You can also watch the progress on [twitter](https://twitter.com/year_progress)
 ---
 
-[![Progress Bar CI](https://github.com/thatoranzhevyy/thatoranzhevyy/actions/workflows/node.js.yml/badge.svg)](https://github.com/thatoranzhevyy/thatoranzhevyy/actions/workflows/node.js.yml)
+[![Node.js CI](https://github.com/thatoranzhevyy/thatoranzhevyy/actions/workflows/node.js.yml/badge.svg?branch=master&event=schedule)](https://github.com/thatoranzhevyy/thatoranzhevyy/actions/workflows/node.js.yml)
 `
 
 console.log(readme)
